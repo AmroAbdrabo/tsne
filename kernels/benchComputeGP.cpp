ce4fea72b8a63fa253689dc9ca222a7a050d3048
Amro Abdrabo <aabdrabo@student.ethz.ch>
2021-05-05 18:36:58 +0200

    change format of runtime file

--- kernels/benchComputeGP.cpp
+++ kernels/benchComputeGP.cpp
@@ -15,37 +15,6 @@
 #define CALIBRATE
 
 
-/*double scalar_product(double *x, double *y, int n){
-    
-    double s = 0.0;
-    double s2 = 0.0;
-    double s3 = 0.0;
-    double s4 = 0.0;
-    double s5 = 0.0;
-    double s6 = 0.0;
-
-    int limit = n - 5;
-    int i;
-    for (i = 0 ; i < limit; i+=6){
-        s += x[i]*y[i];
-        s2 += x[i+1]*y[i+1];
-        s3 += x[i+2]*y[i+2];
-        s4 += x[i+3]*y[i+3];
-        s5 += x[i+4]*y[i+4];
-        s6 += x[i+5]*y[i+5];
- 
-    }
-    for (; i< n; ++i){
-        s+=x[i]*y[i];
-    }
-    return (s+s2+s3+s4+s5+s6);
-}*/
-
-
-/*
- * Timing function based on the TimeStep Counter of the CPU.
- */
-
 double rdtsc(const double* X, double* P, int n) {
     int i, num_runs;
     myInt64 cycles;

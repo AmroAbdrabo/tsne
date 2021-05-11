f1b4fb98b13c00342f8fa22d32b7c8e1be0f6f64
Dimitris Koutsoukos <dkoutsou@inf.ethz.ch>
2021-05-11 16:48:17 +0200

    Split by optimization

--- kernels/computeGP.hpp
+++ kernels/computeGP.hpp
@@ -15,6 +15,7 @@
  * @param perp user-defined perplexity to determine the optimal variance
  */
 
+// initial kernel
 namespace computeGPv1 {
 
     inline void computeGaussianPerplexity(const double* X, const size_t N, const unsigned int in_dim, double* P, const double perp) {
@@ -118,7 +119,263 @@ namespace computeGPv1 {
     }
 }
 
+// only math transformation operations
 namespace computeGPv2 {
+
+    inline void computeGaussianPerplexity(const double* X, const size_t N, const unsigned int in_dim, double* P, const double perp) {
+        // Compute the squared Euclidean distance matrix
+        double* DD = (double*) malloc(N * N * sizeof(double));
+        if(DD == NULL) { printf("Memory allocation failed!\n"); exit(1); }
+        computeSEDv1::computeSquaredEuclideanDistance(X, N, in_dim, DD);
+
+        // Compute the Gaussian kernel row by row
+        int nN = 0;
+        for(int n = 0; n < N; n++) {
+
+            // Initialize some variables
+            bool found = false;
+            double beta = 1.0;
+            double min_beta = -DBL_MAX;
+            double max_beta =  DBL_MAX;
+            double tol = 1e-5;
+            double sum_P;
+
+            // Iterate until we found a good perplexity
+            int iter = 0;
+            while(iter < 200) {
+            // while(!found && iter < 200) {
+
+                // Compute Gaussian kernel row
+                for(int m = 0; m < N; m++) {
+                    P[nN + m] = exp(-beta * DD[nN + m]);
+                }
+                P[nN + n] = DBL_MIN;
+
+                // Compute entropy of current row
+                sum_P = DBL_MIN;
+                for(int m = 0; m < N; m++) {
+                    sum_P += P[nN + m];
+                }
+                double H = 0.0;
+                for(int m = 0; m < N; m++) {
+                    H += beta * (DD[nN + m] * P[nN + m]);
+                }
+                H = (H / sum_P) + log(sum_P);
+
+                // Evaluate whether the entropy is within the tolerance level
+                double Hdiff = H - log(perp);
+                if(Hdiff < tol && -Hdiff < tol) {
+                    found = true;
+                }
+                else {
+                    if(Hdiff > 0) {
+                        min_beta = beta;
+                        if (max_beta == DBL_MAX || max_beta == -DBL_MAX) {
+                            beta *= 2.0;
+                        }
+                        else {
+                            beta = (beta + max_beta) / 2.0;
+                        }
+                    }
+                    else {
+                        max_beta = beta;
+                        if(min_beta == -DBL_MAX || min_beta == DBL_MAX) {
+                            beta *= 0.5;
+                        }
+                        else {
+                            beta = (beta + min_beta) * 0.5;
+                        }
+                    }
+                }
+
+                // Update iteration counter
+                iter++;
+            }
+
+            // Row normalize P
+            double inv_sum_P = 1 / sum_P;
+            for(int m = 0; m < N; m++) {
+                P[nN + m] *= inv_sum_P;
+            }
+            nN += N;
+        }
+
+        // Clean up memory
+        free(DD);
+        DD = NULL;
+
+        // Symmetrize input similarities
+        nN = 0;
+        for(int n = 0; n < N; n++) {
+            int mN = (n + 1) * N;
+            for(int m = n + 1; m < N; m++) {
+                P[nN + m] += P[mN + n];
+                P[mN + n]  = P[nN + m];
+                mN += N;
+            }
+            nN += N;
+        }
+        double sum_P = .0;
+        // save computation
+        size_t N_sq = N*N;
+        for(int i = 0; i < N_sq; i++) {
+            sum_P += P[i];
+        }
+        double inv_sum_P = 1 / sum_P;
+        for(int i = 0; i < N_sq; i++) {
+            P[i] *= inv_sum_P;
+        }
+    }
+}
+
+// unrolling and ILP
+namespace computeGPv3 {
+
+    inline void computeGaussianPerplexity(const double* X, const size_t N, const unsigned int in_dim, double* P, const double perp) {
+        // Compute the squared Euclidean distance matrix
+        double* DD = (double*) malloc(N * N * sizeof(double));
+        if(DD == NULL) { printf("Memory allocation failed!\n"); exit(1); }
+        computeSEDv1::computeSquaredEuclideanDistance(X, N, in_dim, DD);
+
+        // Compute the Gaussian kernel row by row
+        int nN = 0;
+        for(int n = 0; n < N; n++) {
+
+            // Initialize some variables
+            bool found = false;
+            double beta = 1.0;
+            double min_beta = -DBL_MAX;
+            double max_beta =  DBL_MAX;
+            double tol = 1e-5;
+            double sum_P;
+
+            // Iterate until we found a good perplexity
+            int iter = 0;
+            while(iter < 200) {
+            // while(!found && iter < 200) {
+
+                // Compute Gaussian kernel row
+                // unrolling and avoid aliasing
+                for(int m = 0; m < N; m+=4) {
+                    #pragma ivdep
+                    P[nN + m] = exp(-beta * DD[nN + m]);
+                    P[nN + m + 1] = exp(-beta * DD[nN + m + 1]);
+                    P[nN + m + 2] = exp(-beta * DD[nN + m + 2]);
+                    P[nN + m + 3] = exp(-beta * DD[nN + m + 3]);
+                }
+                P[nN + n] = DBL_MIN;
+
+                // Compute entropy of current row
+                sum_P = DBL_MIN;
+                double sum1, sum2, sum3, sum4;
+                // unrolling and ILP
+                for(int m = 0; m < N; m+=4) {
+                    sum1 += P[nN + m];
+                    sum2 += P[nN + m + 1];
+                    sum3 += P[nN + m + 2];
+                    sum4 += P[nN + m + 3];
+                }
+                sum_P += sum1 + sum2 + sum3 + sum4;
+                double H = 0.0, h1, h2, h3, h4;
+                // unrolling and ILP
+                for(int m = 0; m < N; m++) {
+                    h1 += beta * (DD[nN + m] * P[nN + m]);
+                    h2 += beta * (DD[nN + m] * P[nN + m]);
+                    h3 += beta * (DD[nN + m] * P[nN + m]);
+                    h4 += beta * (DD[nN + m] * P[nN + m]);
+                }
+                H += h1 + h2 + h3 + h4;
+                H = (H / sum_P) + log(sum_P);
+
+                // Evaluate whether the entropy is within the tolerance level
+                double Hdiff = H - log(perp);
+                if(Hdiff < tol && -Hdiff < tol) {
+                    found = true;
+                }
+                else {
+                    if(Hdiff > 0) {
+                        min_beta = beta;
+                        if (max_beta == DBL_MAX || max_beta == -DBL_MAX) {
+                            beta *= 2.0;
+                        }
+                        else {
+                            beta = (beta + max_beta) / 2.0;
+                        }
+                    }
+                    else {
+                        max_beta = beta;
+                        if(min_beta == -DBL_MAX || min_beta == DBL_MAX) {
+                            beta /= 2.0;
+                        }
+                        else {
+                            beta = (beta + min_beta) / 2.0;
+                        }
+                    }
+                }
+
+                // Update iteration counter
+                iter++;
+            }
+
+            // Row normalize P
+            // unrolling and multiplication into division
+            double inv_sum_P = 1 / sum_P;
+            for(int m = 0; m < N; m+=4) {
+                #pragma ivdep
+                P[nN + m] *= inv_sum_P;
+                P[nN + m + 1] *= inv_sum_P;
+                P[nN + m + 2] *= inv_sum_P;
+                P[nN + m + 3] *= inv_sum_P;
+            }
+            nN += N;
+        }
+
+        // Clean up memory
+        free(DD); DD = NULL;
+
+        // Symmetrize input similarities
+        nN = 0;
+        for(int n = 0; n < N; n++) {
+            int mN = (n + 1) * N;
+            // unrolling,  ILP and scalar replacement
+            double sum1, sum2, sum3, sum4;
+            for(int m = n + 1; m < N; m+=4) {
+                sum1 += P[mN + n];
+                P[mN + n]  = sum1;
+                P[nN + m] = sum1;
+                sum2 += P[mN + n + 1];
+                P[mN + n + 1]  = sum2;
+                P[nN + m + 1] = sum2;
+                sum3 += P[mN + n + 2];
+                P[mN + n + 2]  = sum3;
+                P[nN + m + 2] = sum3;
+                sum4 += P[mN + n + 3];
+                P[mN + n + 3]  = sum4;
+                P[mN + m + 3] = sum4;
+                mN += N;
+            }
+            nN += N;
+        }
+        double sum_P = .0, sum1, sum2, sum3, sum4;
+        // unrolling and ILP
+        for(int i = 0; i < N * N; i+=4) {
+            sum1 += P[i];
+            sum2 += P[i + 1];
+            sum3 += P[i + 2];
+            sum4 += P[i + 3];
+        }
+        sum_P = sum1 + sum2 + sum3 + sum4;
+        for(int i = 0; i < N * N; i+=4) {
+            #pragma ivdep
+            P[i] /= sum_P;
+            P[i + 1] /= sum_P;
+            P[i + 2] /= sum_P;
+            P[i + 3] /= sum_P;
+        }
+    }
+}
+// fully optimized scalar implementation
+namespace computeGPv4 {
 
     inline void computeGaussianPerplexity(const double* X, const size_t N, const unsigned int in_dim, double* P, const double perp) {
         // Compute the squared Euclidean distance matrix

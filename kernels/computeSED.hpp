52efbc4ff5a592dbaa92558af10b2c92c431308d
Bowen Wu <wubo@student.ethz.ch>
2021-05-27 10:46:01 +0200

    change micro-blocking buffering impl.

--- kernels/computeSED.hpp
+++ kernels/computeSED.hpp
@@ -394,7 +394,7 @@ namespace computeSEDv2d2ru{ // with blocking for cache AND register w unrolling 
 namespace computeSEDv2d2rubuf{ // with blocking for cache AND register w unrolling w buffering
     void computeSquaredEuclideanDistance(const double* X, int N,  int D, double* DD) {
        const int b = 32; // block size for cache
-       const int rbi = 4; // block size for registers
+       const int rbi = 8; // block size for registers
        double buf[b * b] __attribute__ ((aligned (32)));
 
 
@@ -433,79 +433,77 @@ namespace computeSEDv2d2rubuf{ // with blocking for cache AND register w unrolli
                     double x1d0 = Xii[2], x1d1 = Xii[3];
                     double x2d0 = Xii[4], x2d1 = Xii[5];
                     double x3d0 = Xii[6], x3d1 = Xii[7];
+                    double x4d0 = Xii[8], x4d1 = Xii[9];
+                    double x5d0 = Xii[10], x5d1 = Xii[11];
+                    double x6d0 = Xii[12], x6d1 = Xii[13];
+                    double x7d0 = Xii[14], x7d1 = Xii[15];
                     
-                    for(int jj = j; jj < j + b - 1; jj += 2, Xjj += D * 2) {
-                            double tmp1, tmp2, dist, tmp3, tmp4, ddist; // dynamic register renaming
-                            
+                    for(int jj = j; jj < j + b; jj += 1, Xjj += D) {
                             // stay in registers for reuse
                             double yd0 = Xjj[0];
                             double yd1 = Xjj[1];
-                            double yyd0 = Xjj[2];
-                            double yyd1 = Xjj[3];
 
-                            int base = ii * N + jj;
-                            int buf_base = (jj-j) * b + ii - i;
-                            int bbase = base + 1; 
-                            int bbuf_base = buf_base + b; 
+                            int buf_base = (ii-i) * b + jj - j;
+                            int base = jj * N + ii;
                             
-                            tmp1 = x0d0 - yd0;
-                            tmp2 = x0d1 - yd1;
-                            tmp3 = x0d0 - yyd0;
-                            tmp4 = x0d1 - yyd1;
-                            dist = tmp1 * tmp1 + tmp2 * tmp2;
-                            ddist = tmp3 * tmp3 + tmp4 * tmp4;
-                            DD[base] = dist;
-                            buf[buf_base] = dist;
-                            DD[bbase] = ddist;
-                            buf[bbuf_base] = ddist;
-                            base += N;
-                            bbase += N;
+                            double tmp1 = x0d0 - yd0;
+                            double tmp2 = x0d1 - yd1;
+                            double dist1 = tmp1 * tmp1 + tmp2 * tmp2;
                             
-                            tmp1 = x1d0 - yd0;
-                            tmp2 = x1d1 - yd1;
-                            tmp3 = x1d0 - yyd0;
-                            tmp4 = x1d1 - yyd1;
-                            dist = tmp1 * tmp1 + tmp2 * tmp2;
-                            ddist = tmp3 * tmp3 + tmp4 * tmp4;
-                            DD[base] = dist;
-                            buf[buf_base + 1] = dist;
-                            DD[bbase] = ddist;
-                            buf[bbuf_base + 1] = ddist;
-                            base += N;
-                            bbase += N;
+                            double tmp3 = x1d0 - yd0;
+                            double tmp4 = x1d1 - yd1;
+                            double dist2 = tmp3 * tmp3+ tmp4 * tmp4;
+                            
+                            double tmp5 = x2d0 - yd0;
+                            double tmp6 = x2d1 - yd1;
+                            double dist3 = tmp5 * tmp5 + tmp6 * tmp6;
 
-                            tmp1 = x2d0 - yd0;
-                            tmp2 = x2d1 - yd1;
-                            tmp3 = x2d0 - yyd0;
-                            tmp4 = x2d1 - yyd1;
-                            dist = tmp1 * tmp1 + tmp2 * tmp2;
-                            ddist = tmp3 * tmp3 + tmp4 * tmp4;
-                            DD[base] = dist;
-                            buf[buf_base + 2] = dist;
-                            DD[bbase] = ddist;
-                            buf[bbuf_base + 2] = ddist;
-                            base += N;
-                            bbase += N;
+                            double tmp7 = x3d0 - yd0;
+                            double tmp8 = x3d1 - yd1;
+                            double dist4 = tmp7 * tmp7 + tmp8 * tmp8;
 
-                            tmp1 = x3d0 - yd0;
-                            tmp2 = x3d1 - yd1;
-                            tmp3 = x3d0 - yyd0;
-                            tmp4 = x3d1 - yyd1;
-                            dist = tmp1 * tmp1 + tmp2 * tmp2;
-                            ddist = tmp3 * tmp3 + tmp4 * tmp4;
-                            DD[base] = dist;
-                            buf[buf_base + 3] = dist;
-                            DD[bbase] = ddist;
-                            buf[bbuf_base + 3] = ddist;
+                            double tmp9 = x4d0 - yd0;
+                            double tmp10 = x4d1 - yd1;
+                            double dist5 = tmp9 * tmp9 + tmp10 * tmp10;
+                            
+                            double tmp11 = x5d0 - yd0;
+                            double tmp12 = x5d1 - yd1;
+                            double dist6 = tmp11 * tmp11+ tmp12 * tmp12;
+                            
+                            double tmp13 = x6d0 - yd0;
+                            double tmp14 = x6d1 - yd1;
+                            double dist7 = tmp13 * tmp13 + tmp14 * tmp14;
+
+                            double tmp15 = x7d0 - yd0;
+                            double tmp16 = x7d1 - yd1;
+                            double dist8 = tmp15 * tmp15 + tmp16 * tmp16;
+                            
+                            DD[base] = dist1;
+                            DD[base + 1] = dist2;
+                            DD[base + 2] = dist3;
+                            DD[base + 3] = dist4;
+                            DD[base + 4] = dist5;
+                            DD[base + 5] = dist6;
+                            DD[base + 6] = dist7;
+                            DD[base + 7] = dist8;
+
+                            buf[buf_base] = dist1; buf_base += b;
+                            buf[buf_base] = dist2; buf_base += b;
+                            buf[buf_base] = dist3; buf_base += b;
+                            buf[buf_base] = dist4; buf_base += b;
+                            buf[buf_base] = dist5; buf_base += b;
+                            buf[buf_base] = dist6; buf_base += b;
+                            buf[buf_base] = dist7; buf_base += b;
+                            buf[buf_base] = dist8; 
                     }
                 }
 
                 // copy the buffer back to DD
-                for(int jj = j; jj < j + b; jj++) { 
-                    int base = jj * N;
-                    int buf_base = (jj - j) * b;
-                    for(int ii = i; ii < i + b; ii++) {
-                        DD[base + ii] = buf[buf_base + ii - i];
+                for(int ii = i; ii < i + b; ii++) {
+                    int base = ii * N;
+                    int buf_base = (ii - i) * b;
+                    for(int jj = j; jj < j + b; jj++) { 
+                        DD[base + jj] = buf[buf_base + jj - j];
                     }
                 }
             }
